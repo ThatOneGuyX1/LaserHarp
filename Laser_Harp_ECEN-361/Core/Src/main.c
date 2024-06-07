e3175b03e5df04559dd49ab7ef8a6f3bc68717b3
ThatOneGuyX1 <iansearle0@gmail.com>
2024-06-06 22:30:18 -0600

    12 Notes Done, Interuptps and Timer need verification

--- Laser_Harp_ECEN-361/Core/Src/main.c
+++ Laser_Harp_ECEN-361/Core/Src/main.c
@@ -47,18 +47,18 @@
 /* USER CODE BEGIN PM */
 
 /* These defines represent the point in a list where that notes data is held.*/
-#define NOTE_C 0
-#define NOTE_Cs 1
-#define NOTE_D 2
-#define NOTE_Ds 3
-#define NOTE_E 4
-#define NOTE_F 5
-#define NOTE_Fs 6
-#define NOTE_G 7
-#define NOTE_Gs 8
-#define NOTE_A 9
-#define NOTE_As10
-#define NOTE_B 11
+#define NOTE_C 		0
+#define NOTE_Cs 	1
+#define NOTE_D 		2
+#define NOTE_Ds 	3
+#define NOTE_E 		4
+#define NOTE_F 		5
+#define NOTE_Fs 	6
+#define NOTE_G 		7
+#define NOTE_Gs 	8
+#define NOTE_A 		9
+#define NOTE_As		10
+#define NOTE_B 		11
 
 
 /* USER CODE END PM */
@@ -74,7 +74,7 @@ UART_HandleTypeDef huart2;
 /* Index and Active are lists that are 12 long. This is to improve spatial locality in the program.
 Using the above stated define we can thats notes data from the list*/
 int index[]= {0,0,0,0,0,0,0,0,0,0,0,0};
-int active[] = {1,1,0,1,0,0,0,0,0,0,0,0};
+int active[] = {1,0,1,0,0,0,0,0,0,0,0,0};
 
 int wave_out= 0;
 
@@ -266,9 +266,9 @@ static void MX_TIM15_Init(void)
 
   /* USER CODE END TIM15_Init 1 */
   htim15.Instance = TIM15;
-  htim15.Init.Prescaler = 79;
+  htim15.Init.Prescaler = 80-1;
   htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim15.Init.Period = 1000;
+  htim15.Init.Period = 10;
   htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim15.Init.RepetitionCounter = 0;
   htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
@@ -351,8 +351,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : NOTE_1_Pin NOTE_2_Pin NOTE_3_Pin */
-  GPIO_InitStruct.Pin = NOTE_1_Pin|NOTE_2_Pin|NOTE_3_Pin;
+  /*Configure GPIO pins : NOTE_1_Pin NOTE_2_Pin NOTE_3_Pin NOTE_5_Pin
+                           NOTE_6_Pin NOTE_7_Pin NOTE_8_Pin NOTE_9_Pin
+                           NOTE_10_Pin NOTE_11_Pin NOTE_12_Pin */
+  GPIO_InitStruct.Pin = NOTE_1_Pin|NOTE_2_Pin|NOTE_3_Pin|NOTE_5_Pin
+                          |NOTE_6_Pin|NOTE_7_Pin|NOTE_8_Pin|NOTE_9_Pin
+                          |NOTE_10_Pin|NOTE_11_Pin|NOTE_12_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
@@ -373,34 +377,52 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-/*
+	switch (GPIO_Pin) {
+	case NOTE_1_Pin:
+		active[NOTE_C] = !active[NOTE_C];
+		break;
+	case NOTE_2_Pin:
+		active[NOTE_Cs] = !active[NOTE_Cs];
+		break;
+	case NOTE_3_Pin:
+		active[NOTE_D] = !active[NOTE_D];
+		break;
+	case NOTE_4_Pin:
+		active[NOTE_Ds] = !active[NOTE_Ds];
+		break;
+	case NOTE_5_Pin:
+		active[NOTE_E] = !active[NOTE_E];
+		break;
+	case NOTE_6_Pin:
+		active[NOTE_F] = !active[NOTE_F];
+		break;
+	case NOTE_7_Pin:
+		active[NOTE_Fs] = !active[NOTE_Fs];
+		break;
+	case NOTE_8_Pin:
+		active[NOTE_G] = !active[NOTE_G];
+		break;
+	case NOTE_9_Pin:
+		active[NOTE_Gs] = !active[NOTE_Gs];
+		break;
+	case NOTE_10_Pin:
+		active[NOTE_A] = !active[NOTE_A];
+		break;
+	case NOTE_11_Pin:
+		active[NOTE_As] = !active[NOTE_As];
+		break;
+	case NOTE_12_Pin:
+		active[NOTE_B] = !active[NOTE_B];
+		break;
 
-    _   __        __     _       __              __    _                  __ __
-   / | / /____   / /_   | |     / /____   _____ / /__ (_)____   ____ _   / // /
-  /  |/ // __ \ / __/   | | /| / // __ \ / ___// //_// // __ \ / __ `/  / // / 
- / /|  // /_/ // /_     | |/ |/ // /_/ // /   / ,<  / // / / // /_/ /  /_//_/  
-/_/ |_/ \____/ \__/     |__/|__/ \____//_/   /_/|_|/_//_/ /_/ \__, /  (_)(_)   
-                                                             /____/            
-
-*/
-	if(GPIO_Pin == NOTE_1_Pin){
-		active[NOTE_C] = active[NOTE_C]?0:1;
+	default:
+		break;
 	}
-	if(GPIO_Pin == NOTE_2_Pin){
-			active[NOTE_Cs] = active[NOTE_Cs]?0:1;
-		}
-	if(GPIO_Pin == NOTE_3_Pin){
-			active[NOTE_D] = active[NOTE_D]?0:1;
-		}
-	if(GPIO_Pin == NOTE_4_Pin){
-			active[NOTE_Ds] = active[NOTE_Ds]?0:1;
-		}
 }
 
 // Callback: timer has rolled over
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  // Check which version of the timer triggered this callback and toggle LED
   if (htim == &htim15 )
   {
     /* ***************************************************************
@@ -409,9 +431,18 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     * If the note is active then we update the value. Otherwise we return a zero.
     ***************************************************************************/
 	  int C_val = 	active[NOTE_C]?C_TABLE[index[NOTE_C]]:0;
-	  int Cs_val = 	active[NOTE_Cs]?C_SHARP_TABLE[index[NOTE_Cs]]:0;
+	  int Cs_val = 	active[NOTE_Cs]?Cs_TABLE[index[NOTE_Cs]]:0;
 	  int D_val = 	active[NOTE_D]?D_TABLE[index[NOTE_D]]:0;
-	  int Ds_val = 	active[NOTE_Ds]?D_SHARP_TABLE[index[NOTE_Ds]]:0;
+	  int Ds_val = 	active[NOTE_Ds]?Ds_TABLE[index[NOTE_Ds]]:0;
+	  int E_val = 	active[NOTE_E]?E_TABLE[index[NOTE_E]]:0;
+	  int F_val = 	active[NOTE_F]?F_TABLE[index[NOTE_F]]:0;
+	  int Fs_val = 	active[NOTE_Fs]?Fs_TABLE[index[NOTE_Fs]]:0;
+	  int G_val = 	active[NOTE_G]?G_TABLE[index[NOTE_G]]:0;
+	  int Gs_val = 	active[NOTE_Gs]?Gs_TABLE[index[NOTE_Gs]]:0;
+	  int A_val = 	active[NOTE_A]?A_TABLE[index[NOTE_A]]:0;
+	  int As_val = 	active[NOTE_As]?As_TABLE[index[NOTE_As]]:0;
+	  int B_val = 	active[NOTE_B]?B_TABLE[index[NOTE_B]]:0;
+
 
     /* ***************************************************************
     This batch of code is to just update the index if the note is active.
@@ -421,9 +452,17 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	  index[NOTE_Cs] += 	active[NOTE_Cs];
 	  index[NOTE_D]	+= 	active[NOTE_D];
 	  index[NOTE_Ds] += 	active[NOTE_Ds];
+    index[NOTE_E] += 	active[NOTE_E];
+    index[NOTE_F] += 	active[NOTE_F];
+    index[NOTE_Fs] += 	active[NOTE_Fs];
+    index[NOTE_G] += 	active[NOTE_G];
+    index[NOTE_Gs] += 	active[NOTE_Gs];
+    index[NOTE_A] += 	active[NOTE_A];
+    index[NOTE_As] += 	active[NOTE_As];
+    index[NOTE_B] += 	active[NOTE_B];
 
     
-	  wave_out = C_val+Cs_val+Ds_val+D_val; // Calculate the Sum of the wave
+	  wave_out = C_val+Cs_val+Ds_val+D_val+E_val+F_val+Fs_val+G_val+Gs_val+A_val+As_val+B_val; // Calculate the Sum of the wave
 	  int temp_wave = wave_out * 12/4; // This is for debugging purposes. Also 12/4 is to create the new offset for only using 4 notes
 	  
     HAL_DAC_SetValue(&hdac1, DAC1_CHANNEL_2, DAC_ALIGN_12B_L , temp_wave); // Output the wave
@@ -435,6 +474,15 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	  if(index[NOTE_Cs] > Cs_SAMPLES) index[NOTE_Cs] =0;
 	  if(index[NOTE_D] > D_SAMPLES) index[NOTE_D] =0;
 	  if(index[NOTE_Ds] > Ds_SAMPLES) index[NOTE_Ds] =0;
+    if(index[NOTE_E] > E_SAMPLES) index[NOTE_E] =0;
+    if(index[NOTE_F] > F_SAMPLES) index[NOTE_F] =0;
+    if(index[NOTE_Fs] > Fs_SAMPLES) index[NOTE_Fs] =0;
+    if(index[NOTE_G] > G_SAMPLES) index[NOTE_G] =0;
+    if(index[NOTE_Gs] > Gs_SAMPLES) index[NOTE_Gs] =0;
+    if(index[NOTE_A] > A_SAMPLES) index[NOTE_A] =0;
+    if(index[NOTE_As] > As_SAMPLES) index[NOTE_As] =0;
+    if(index[NOTE_B] > B_SAMPLES) index[NOTE_B] =0;
+
   }
 }
 /* USER CODE END 4 */
